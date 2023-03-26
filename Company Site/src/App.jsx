67e2cafd57b2e18e783d236744643820bdd0cd50
Mritunjay Kumar <akshaysingh906022@gmail.com>
2023-03-26 13:31:04 +0530

    update

--- Company Site/src/App.jsx
+++ Company Site/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import About from './Components/Navbar/About'
+import { Route,Routes } from 'react-router-dom'
 
 function App() {
  
   return (
     <div className="App">
       <Navbar/>
+      {/* <About /> */}
     </div>
   )
 }
