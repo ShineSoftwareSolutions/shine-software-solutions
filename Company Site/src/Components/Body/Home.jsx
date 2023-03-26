dbd3a24f6750bf1a7db78e2e946a419830e96dcb
ShravaniMishra <shravniguddi@gmail.com>
2023-03-27 03:46:31 +0530

    part-1 added

--- Company Site/src/Components/Body/Home.jsx
+++ Company Site/src/Components/Body/Home.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import './Home.css'
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 const Home = () => {
   return (
     <>
-    <div></div>
+      <div className="main_box">
+        <div className="subpart1">
+          <img
+            src="https://images.pexels.com/photos/306763/pexels-photo-306763.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="apple"
+          />
+        </div>
+        <div className="subpart2">
+          <h2>
+            "Make something people want” includes making a company that people
+            want to work for.
+          </h2>
+          <p>
+            We are here to serve the requirements what you actually looking for.
+          </p>
+          <button className="btn btn-outline-secondary">Hit me</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Home
