60110f3c857f514fbd317ae6850de211aad40c29
ShravaniMishra <shravniguddi@gmail.com>
2023-03-29 21:41:11 +0530

    Section4

--- Company Site/src/Components/Body/Home.jsx
+++ Company Site/src/Components/Body/Home.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import './Home.css'
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import "./Home.css";
+// import { Link } from 'react-router-dom'
+import "../../../node_modules/bootstrap/dist/css/bootstrap.rtl.min.css";
 
 const Home = () => {
   return (
@@ -45,8 +46,68 @@ const Home = () => {
           understand their needs and goals.
         </h2>
       </div>
+
+      <div className="Section3">
+        <img
+          src="https://www.michaelpage.com.au/sites/michaelpage.com.au/files/legacy/future_of_tech_bloggif.gif"
+          alt="BannerType"
+        />
+      </div>
+      <div className="Section4">
+        <div className="card">
+          <img
+            src="https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&cs=tinysrgb&w=600"
+            className="card-img-top"
+            alt="web development"
+          />
+          <div className="card-body">
+            <p className="card-text">
+              Specialize in full-stack development and focuses on the visual aspects of a website and the user experience .
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src="https://images.pexels.com/photos/54284/pexels-photo-54284.jpeg?auto=compress&cs=tinysrgb&w=600"
+            className="card-img-top"
+            alt="Mobile Application"
+          />
+          <div className="card-body">
+            <p className="card-text">
+              Apps are built for specific mobile operating systems and popular
+              in recent years.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src="https://images.pexels.com/photos/461064/pexels-photo-461064.jpeg?auto=compress&cs=tinysrgb&w=600"
+            className="card-img-top"
+            alt="web design"
+          />
+          <div className="card-body">
+            <p className="card-text">
+              creating and designing the visual elements and overall layout of
+              websites and its goals.
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            src="https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=600"
+            className="card-img-top"
+            alt="UI/UX Development"
+          />
+          <div className="card-body">
+            <p className="card-text">
+              UI development involves designing and building the visual and
+              interactive elements of the product.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
-}
+};
 
-export default Home
+export default Home;
